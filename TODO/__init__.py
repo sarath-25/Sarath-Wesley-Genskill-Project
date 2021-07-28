f0fa7d42dababf9b0905f6c25ec796280c746e63
Sarath Wesley <wesleysarath1@gmail.com>
2021-07-28 21:11:22 +0530

    Adds home page and secret key to the app

--- TODO/__init__.py
+++ TODO/__init__.py
@@ -1,14 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, g
+import os
 
 def create_app():
-    app = Flask("TODO List Manager")
+    app = Flask("TODO")
     app.config.from_mapping(DATABASE="Manager")
+    app.secret_key = os.urandom(24)
+
     
-    
-    from . import TODO
-    app.register_blueprint(TODO.bp)
+    from . import todo
+    app.register_blueprint(todo.bp)
     
     from . import db
     db.init_app(app)
     
+    
+    
+    
+    @app.route("/")
+    def index():
+        return render_template('index.html')
+    
+    
     return app
+    
